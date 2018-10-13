693300d54c8866f1839f4f3580932b5370b3db23
calvinCar <m17610602708@163.com>
2018-10-13 13:01:48 +0800

    data

--- pages/js/const/api.js
+++ pages/js/const/api.js
@@ -3,22 +3,15 @@ const API_PREFIX = 'http://mock.sina.cn/api/';
 const MOCK_API_1 = API_PREFIX + '1';
 
 // MOCK_API_1的Mock响应模板，详细用法参见：https://github.com/nuysoft/Mock/wiki
-var obj = {'aa':'11', 'bb':'22', 'cc':'33', 'dd':'44'};
+// var obj = {'aa':'11', 'bb':'22', 'cc':'33', 'dd':'44'};
 Mock.mock(MOCK_API_1, 'get', {
-    "user|1-2": [{   // 随机生成1到3个数组元素
+    "data|100": [{   // 随机生成1到3个数组元素
         'name': '@cname',  // 中文名称
         'id|+1': 88,    // 属性值自动加 1，初始值为88
-        'age|18-28': 0,   // 18至28以内随机整数, 0只是用来确定类型
-        'birthday': '@date("yyyy-MM-dd")',  // 日期
-        'city': '@city(true)',   // 中国城市
-        'color': '@color',  // 16进制颜色
-        'isMale|1': true,  // 布尔值
-        'isFat|1-2': true,  // true的概率是1/3
-        'fromObj|2': obj,  // 从obj对象中随机获取2个属性
-        'fromObj2|1-3': obj,  // 从obj对象中随机获取1至3个属性
-        'brother|1': ['jack', 'jim'], // 随机选取 1 个元素
-        'sister|+1': ['jack', 'jim', 'lily'], // array中顺序选取元素作为结果
-        'friends|2': ['jack', 'jim'] // 重复2次属性值生成一个新数组
+        'select0|1': ['1号楼', '2号楼','3号楼'], 
+        'select1|1': ['1单元', '2单元','3单元'], 
+        'select2|1': ['高区', '中区','低区'], 
+        'select3|1': ['类型1', '类型2','类型3'], 
     }]
 });
 
---
+++ pages/js/const/data.js
@@ -0,0 +1,804 @@
+module.exports ={
+    "data": [
+        {
+            "name": "邹芳",
+            "id": 88,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "卢娟",
+            "id": 89,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "邵强",
+            "id": 90,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "高洋",
+            "id": 91,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型3"
+        },
+        {
+            "name": "蒋秀英",
+            "id": 92,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "汪超",
+            "id": 93,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "廖平",
+            "id": 94,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "史敏",
+            "id": 95,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "邹桂英",
+            "id": 96,
+            "select0": "3号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "陆敏",
+            "id": 97,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "吴敏",
+            "id": 98,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型1"
+        },
+        {
+            "name": "孔勇",
+            "id": 99,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "邱军",
+            "id": 100,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "沈勇",
+            "id": 101,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型3"
+        },
+        {
+            "name": "萧芳",
+            "id": 102,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "冯娟",
+            "id": 103,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "贺桂英",
+            "id": 104,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "杨芳",
+            "id": 105,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "周杰",
+            "id": 106,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "任平",
+            "id": 107,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "梁杰",
+            "id": 108,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "杜伟",
+            "id": 109,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "文杰",
+            "id": 110,
+            "select0": "3号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "沈杰",
+            "id": 111,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "萧霞",
+            "id": 112,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型3"
+        },
+        {
+            "name": "白敏",
+            "id": 113,
+            "select0": "3号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "黄军",
+            "id": 114,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "乔涛",
+            "id": 115,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "吴磊",
+            "id": 116,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "刘磊",
+            "id": 117,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "邓霞",
+            "id": 118,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "程娜",
+            "id": 119,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "邓勇",
+            "id": 120,
+            "select0": "3号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型3"
+        },
+        {
+            "name": "陈娟",
+            "id": 121,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "马静",
+            "id": 122,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "易杰",
+            "id": 123,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "高区",
+            "select3": "类型3"
+        },
+        {
+            "name": "贺磊",
+            "id": 124,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "吕静",
+            "id": 125,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "曾明",
+            "id": 126,
+            "select0": "1号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "武平",
+            "id": 127,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "孟霞",
+            "id": 128,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "薛芳",
+            "id": 129,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "易敏",
+            "id": 130,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "常杰",
+            "id": 131,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "徐刚",
+            "id": 132,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "贺伟",
+            "id": 133,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型3"
+        },
+        {
+            "name": "郑军",
+            "id": 134,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型3"
+        },
+        {
+            "name": "谢军",
+            "id": 135,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "潘磊",
+            "id": 136,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "魏秀英",
+            "id": 137,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "田明",
+            "id": 138,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "夏强",
+            "id": 139,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "郭秀兰",
+            "id": 140,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "贺勇",
+            "id": 141,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "姚丽",
+            "id": 142,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "赖军",
+            "id": 143,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "郭秀英",
+            "id": 144,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "钱芳",
+            "id": 145,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "方娟",
+            "id": 146,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "低区",
+            "select3": "类型1"
+        },
+        {
+            "name": "唐刚",
+            "id": 147,
+            "select0": "1号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "方刚",
+            "id": 148,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "曾军",
+            "id": 149,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "戴磊",
+            "id": 150,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "谭芳",
+            "id": 151,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "唐超",
+            "id": 152,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "贾娜",
+            "id": 153,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型1"
+        },
+        {
+            "name": "易芳",
+            "id": 154,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "吕秀兰",
+            "id": 155,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "秦艳",
+            "id": 156,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "贺明",
+            "id": 157,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "余刚",
+            "id": 158,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "常磊",
+            "id": 159,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "龚丽",
+            "id": 160,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "郭明",
+            "id": 161,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "侯磊",
+            "id": 162,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "蒋丽",
+            "id": 163,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "苏秀英",
+            "id": 164,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "龚秀英",
+            "id": 165,
+            "select0": "3号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型1"
+        },
+        {
+            "name": "康丽",
+            "id": 166,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "李军",
+            "id": 167,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "许秀英",
+            "id": 168,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "低区",
+            "select3": "类型3"
+        },
+        {
+            "name": "孔明",
+            "id": 169,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "周秀兰",
+            "id": 170,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "傅静",
+            "id": 171,
+            "select0": "3号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "雷勇",
+            "id": 172,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "贾丽",
+            "id": 173,
+            "select0": "2号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "唐芳",
+            "id": 174,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "刘强",
+            "id": 175,
+            "select0": "1号楼",
+            "select1": "1单元",
+            "select2": "低区",
+            "select3": "类型1"
+        },
+        {
+            "name": "董桂英",
+            "id": 176,
+            "select0": "1号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型3"
+        },
+        {
+            "name": "顾军",
+            "id": 177,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "任敏",
+            "id": 178,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "薛桂英",
+            "id": 179,
+            "select0": "1号楼",
+            "select1": "3单元",
+            "select2": "低区",
+            "select3": "类型2"
+        },
+        {
+            "name": "吴敏",
+            "id": 180,
+            "select0": "1号楼",
+            "select1": "1单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "郝丽",
+            "id": 181,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "傅杰",
+            "id": 182,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "低区",
+            "select3": "类型3"
+        },
+        {
+            "name": "赵娟",
+            "id": 183,
+            "select0": "3号楼",
+            "select1": "2单元",
+            "select2": "中区",
+            "select3": "类型1"
+        },
+        {
+            "name": "秦军",
+            "id": 184,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "侯娟",
+            "id": 185,
+            "select0": "2号楼",
+            "select1": "2单元",
+            "select2": "高区",
+            "select3": "类型2"
+        },
+        {
+            "name": "乔娟",
+            "id": 186,
+            "select0": "2号楼",
+            "select1": "3单元",
+            "select2": "中区",
+            "select3": "类型2"
+        },
+        {
+            "name": "曾桂英",
+            "id": 187,
+            "select0": "1号楼",
+            "select1": "1单元",
+            "select2": "低区",
+            "select3": "类型2"
+        }
+    ]
+}
